07cdf3420aba1188623b8c842a41e39db5794318
hugito31200 <hugo.teixeiradecastro@hotmail.com>
2022-02-14 08:34:44 +0100

    Added source code

---
+++ Code/automate.h
@@ -0,0 +1,25 @@
+//---------- Interface de la classe <Automate> (fichier Automate.h) ----------------
+#if ! defined ( AUTOMATE_H )
+#define AUTOMATE_H
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class Automate 
+{
+protected:
+	stack<Etat> pileEtat;
+
+public:
+
+    Automate ( );
+
+    virtual ~Automate ( );
+	
+	virtu
+
+};
+
+#endif // AUTOMATE_H
---
+++ Code/etat.h
@@ -0,0 +1,31 @@
+//---------- Interface de la classe <Etat> (fichier Etat.h) ----------------
+#if ! defined ( Etat_H )
+#define Etat_H
+
+#include "automate.h"
+#include "symbole.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Etat 
+{
+protected:
+
+	string name;
+
+public:
+
+    Etat (const string & name);
+
+    virtual ~Etat ( );
+	
+	void print() const;
+	
+	virtual bool transition(Automate & unAutomate, Symbole * s)=0;
+
+};
+
+#endif // Etat_H
