e443fbcde040cde3e30a7f0c61bd4733e5908a4a
hugito31200 <hugo.teixeiradecastro@hotmail.com>
2022-02-14 10:38:59 +0100

    Implemented e6.cpp

---
+++ Code/e6.cpp
@@ -0,0 +1,18 @@
+#include "e5.h"
+#include <string>
+
+bool E5::transition(Automate * a, Symbole * s){
+	
+	 switch(*s){
+		case PLUS:
+			a->decalage(s, new E4("etat4"));
+			break;
+		case CLOSEPAR:
+			a->decalage(s,new E9("etat9"));
+			break;
+		case MULT:
+			a->decalage(s,new E5("etat5"));
+			break;
+	 } 
+	return false;
+}
