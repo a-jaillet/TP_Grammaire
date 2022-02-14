aad9a2594394cc65e1d62ce7816575bd83ce4b64
Adrien Jaillet <adrien.jaillet@insa-lyon.fr>
2022-02-14 11:26:27 +0100

    Fix syntax

--- Code/e3.cpp
+++ Code/e3.cpp
@@ -4,7 +4,7 @@ bool E3::transition(Automate * a, Symbole * s){
 	
 	switch(*s) {
         Entier * e = (Entier *) a->popSymbol();
-        Expr * exp  = new Expr(e->getValeur())
+        Expr * exp  = new Expr(e->getValeur());
 
 		a->reduction(1,exp);
 		break;
