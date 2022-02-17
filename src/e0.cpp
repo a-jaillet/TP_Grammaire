7fb9f6462ada8ce11e8706113edbe2d79c88bebe
hugito31200 <hugo.teixeiradecastro@hotmail.com>
2022-02-17 14:40:20 +0100

    Added break in switch statement

--- src/e0.cpp
+++ src/e0.cpp
@@ -5,10 +5,13 @@ bool E0::transition(Automate * a, Symbole * s){
 	switch(*s){
 		case EXPR:
 			a->decalage(s, new E1("etat1"));
+			break;
         case OPENPAR:
             a->decalage(s, new E2("etat2"));
+			break;
         case INT:
             a->decalage(s, new E3("etat3"));
+			break;
 	}
 
 	return false;
--- src/e2.cpp
+++ src/e2.cpp
@@ -5,10 +5,13 @@ bool E2::transition(Automate * a, Symbole * s){
 	switch(*s){
         case OPENPAR:
             a -> decalage(s, new E2("etat2"));
+			break;
         case INT:
             a -> decalage(s, new E3("etat3"));
+			break;
         case EXPR:
             a -> decalage(s, new E6("etat6"));
+			break;
 	}
 
 	return false;
